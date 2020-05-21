8297303b997846ce1b22dead60c093ebef4010fb
Aviraj Singh <asingh9@uw.edu>
2020-05-20 18:46:13 -0700

    style check complete for summary_info, summary_table, and third_chart

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -2,7 +2,10 @@ library(dplyr)
 
 rm(list = ls())
 
-cali_hs_sat_zip_df <- read.csv("../data/cali_hs_sat_zip.csv", stringsAsFactors = FALSE)
+cali_hs_sat_zip_df <- read.csv("../data/cali_hs_sat_zip.csv",
+  stringsAsFactors
+  = FALSE
+)
 
 summarise <- function(dataset) {
   output <- list()
@@ -10,12 +13,25 @@ summarise <- function(dataset) {
   median_income <- median(dataset[[7]], na.rm = TRUE)
   output$mean_household_income <- mean_income
   output$median_household_income <- median_income
-  output$avg_score_for_income_below_mean <- mean(dataset[dataset[[7]] < mean_income, 6], na.rm = TRUE)
-  output$avg_score_for_income_atleast_mean <- mean(dataset[dataset[[7]] >= mean_income, 6], na.rm = TRUE)
-  output$avg_score_for_income_below_median <- mean(dataset[dataset[[7]] < median_income, 6], na.rm = TRUE)
-  output$avg_score_for_mincome_atleast_median <- mean(dataset[dataset[[7]] >= median_income, 6], na.rm = TRUE)
-  
+  output$avg_score_below_mean_inc <- mean(dataset[dataset[[7]] <
+    mean_income, 6],
+  na.rm = TRUE
+  )
+  output$avg_score_above_mean_inc <- mean(dataset[dataset[[7]]
+  >= mean_income, 6],
+  na.rm = TRUE
+  )
+  output$avg_score_below_med_inc <- mean(dataset[dataset[[7]] <
+    median_income, 6],
+  na.rm = TRUE
+  )
+  output$avg_score_above_med_inc <- mean(dataset[
+    dataset[[7]] >=
+      median_income,
+    6
+  ], na.rm = TRUE)
+
   return(output)
 }
 
-summary_info <- summarise(cali_hs_sat_zip_df)
+
--- scripts/third_chart.R
+++ scripts/third_chart.R
@@ -4,16 +4,21 @@ library(ggplot2)
 
 rm(list = ls())
 
-cali_hs_sat_zip_df <- read.csv("../data/cali_hs_sat_zip.csv", stringsAsFactors = FALSE)
+cali_hs_sat_zip_df <- read.csv("../data/cali_hs_sat_zip.csv",
+  stringsAsFactors
+  = FALSE
+)
 cali_hs_sat_zip_df <- na.omit(cali_hs_sat_zip_df)
 
-scatter_sat_zip_HS<- ggplot(cali_hs_sat_zip_df,
-                            aes(x = TotalSatScore, y = MedianHouseholdIncome)) +
+scatter_sat_zip_HS <- ggplot(
+  cali_hs_sat_zip_df,
+  aes(x = TotalSatScore, y = MedianHouseholdIncome)
+) +
   geom_point(size = 1, color = "darkblue") +
-  geom_smooth(method=lm) +
+  geom_smooth(method = lm) +
   ggtitle("SAT Score vs ZIP Code Income by HS") +
   xlab("HS Average SAT Score") +
   ylab("Zip Code Median Income")
 
-scatter_sat_zip_HS
-
+style_file("third_chart.R")
+lint("third_chart.R")
