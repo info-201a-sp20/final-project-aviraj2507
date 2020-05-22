c4b10ea8d96d4f0acb90bdf39fb5df0f62fa95d9
Brandon Wong <wongb321@gmail.com>
2020-05-22 11:37:35 -0700

    added script

--- scripts/second_chart.R
+++ scripts/second_chart.R
@@ -1,1 +1,27 @@
 #second chart
+cali_hs_sat_zip_df <- read.csv("data/cali_hs_sat_zip.csv",
+                               stringsAsFactors
+                               = FALSE
+)
+cali_hs_sat_zip_df <- na.omit(cali_hs_sat_zip_df)
+
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+
+
+
+
+plot_ca <- ggplot(data = cali_hs_sat_zip_df) +
+  geom_col(mapping = aes(x = TotalSatScore, y = MedianHouseholdIncome, fill = Zip),
+           position = "dodge")  +
+  ggtitle("SAT Score vs ZIP Code Income by HS") +
+  xlab("HS Average SAT Score") +
+  ylab("Zip Code Median Income")
+
+ggplotly(plot_ca)
+
+
+style_file("second_chart.R")
+lint("second_chart.R")
