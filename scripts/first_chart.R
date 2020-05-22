9dcf4e8e810c4f183306f0222fff45f0a6c629e1
Tony Fei <satouakatsuki@gmail.com>
2020-05-22 15:38:19 -0700

    More meaningful popup text

--- scripts/first_chart.R
+++ scripts/first_chart.R
@@ -3,23 +3,23 @@ library("dplyr")
 library("tidyr")
 library("ggplot2")
 library("leaflet")
+library("lintr")
 
 rm(list = ls())
+map_gen <- function() {
 
-map_gen = function() {
-  
   ca_sat19 <- read.csv("../data/ca-sat19.csv", stringsAsFactors = F)
   ca_income <- read.csv("../data/ca-county_income.csv", stringsAsFactors = F)
   ca_school <- read.csv("../data/ca-pubschls.csv", stringsAsFactors = F)
-  
+
   ca_coord <- select(ca_school, Latitude, Longitude, County) %>%
     group_by(County) %>%
     summarise(lat = mean(as.numeric(Latitude), na.rm = T),
               lng = mean(as.numeric(Longitude), na.rm = T))
-  
+
   ca_income["NameClean"] <- gsub(" County, CA", "", ca_income$Name)
   ca_income["NameClean"] <- gsub(" County/city, CA", "", ca_income$NameClean)
-  
+
   ca_sat19_county <- dplyr::group_by(ca_sat19, CName) %>%
     filter(RType == "C")
   ca_income_sat <- left_join(ca_income, ca_sat19_county,
@@ -29,11 +29,14 @@ map_gen = function() {
   ca_income_sat <- filter(ca_income_sat, !is.na(NumERWBenchmark12)) %>%
     filter(NumTSTTakr12 > 50)
   ca_income_sat <- mutate(ca_income_sat,
-                          "PctERWBenchmark12" = as.numeric(PctERWBenchmark12)) %>%
+                          "PctERWBenchmark12" = as.numeric(
+                            PctERWBenchmark12
+                            )) %>%
     mutate("PctMathBenchmark12" = as.numeric(PctMathBenchmark12)) %>%
     mutate("PctBothBenchmark12" = as.numeric(PctBothBenchmark12))
   ca_income_sat_long <- pivot_longer(ca_income_sat,
-                                     c("PctERWBenchmark12", "PctMathBenchmark12"),
+                                     c("PctERWBenchmark12",
+                                       "PctMathBenchmark12"),
                                      "type")
   ca_income_sat_loc <- left_join(ca_income_sat,
                                  ca_coord, c("NameClean" = "County"))
@@ -45,5 +48,11 @@ map_gen = function() {
   map %>%
     addCircleMarkers(lng = ~lng, lat = ~lat, color = ~ pal(PctBothBenchmark12),
                      fillOpacity = 1,
-                     popup = ~ paste(NameClean, PctBothBenchmark12))
+                     popup = ~ paste0(
+                       NameClean, " County", "<br>",
+                       PctBothBenchmark12,
+                       "% of 12th grade student passed benchmark."
+                       ))
 }
+
+lint("first_chart.R")
