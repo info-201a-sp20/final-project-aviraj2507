8b00b33fbf520430cf166758b0c4ac1dfd3b9863
Brandon Wong <wongb321@gmail.com>
2020-05-22 20:08:28 -0700

    second chart edit

--- scripts/second_chart.R
+++ scripts/second_chart.R
@@ -8,14 +8,18 @@ cali_hs_sat_zip_df <- read.csv("../data/cali_hs_sat_zip.csv",
                                stringsAsFactors
                                = FALSE
 )
-cali_hs_sat_zip_df <- na.omit(cali_hs_sat_zip_df)
+data <- cali_hs_sat_zip_df <- na.omit(cali_hs_sat_zip_df)
 
-plot_ca <- ggplot(data = cali_hs_sat_zip_df) +
+chart_2 <- function(data){
+  ca_vis <- cali_hs_sat_zip_df %>%               
+  ggplot(data = ca_vis) +
   geom_col(mapping = aes(x = TotalSatScore, y = MedianHouseholdIncome, 
                          fill = Zip),position = "dodge")  +
   ggtitle("SAT Score vs ZIP Code Income by HS") +
   xlab("HS Average SAT Score") +
   ylab("Zip Code Median Income")
+p <-ggplotly(plot_ca)
+return(p)
+}
 
-ggplotly(plot_ca)
-
+chart_2(cali_hs_sat_zip_df)
