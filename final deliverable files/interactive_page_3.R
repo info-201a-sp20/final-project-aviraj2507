816ee966cb15985e131634ac9856fd78a98db78f
ejmv1 <ejmv1@uw.edu>
2020-05-30 12:15:04 -0700

    started progress on interactive page three

--- final deliverable files/interactive_page_3.R
+++ final deliverable files/interactive_page_3.R
@@ -1,1 +1,61 @@
+#Creating third page for shiny app
 
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+cali_hs_sat_zip_df <- read.csv("../data/cali_hs_sat_zip.csv",
+                               stringsAsFactors
+                               = FALSE
+)
+
+#Making Scatter Plot
+build_third_plot <- function(dataframe, input){
+  chart <- ggplot(
+    dataframe,
+    aes(x = input, y = MedianHouseholdIncome)
+  ) +
+    geom_point(size = 1, color = "darkblue") +
+    geom_smooth(method = lm) +
+    ggtitle("SAT Score vs ZIP Code Income by HS") +
+    xlab(input) +
+    ylab("Zip Code Median Income")
+  return(ggplotly(chart))
+}
+
+x_axis <- "AvgScrMath"
+
+build_third_plot(cali_hs_sat_zip_df, x_axis)
+
+#Making Binned Bar Graph
+
+binned_income_interactive <- function(dataframe, bin, y_axis) {
+  df <- dataframe %>%
+    filter(MedianHouseholdIncome != is.na(MedianHouseholdIncome)) %>%
+    mutate(income_bin_primary =
+             if(min(floor(MedianHouseholdIncome / bin)) == 0){
+               (floor(MedianHouseholdIncome / bin) + 1)
+             } else {
+                (floor(MedianHouseholdIncome / bin))
+              }) %>%
+    mutate(income_bin =
+             ifelse(income_bin_primary > 10,
+                    10,
+                    income_bin_primary)
+           ) %>%
+    group_by(income_bin) %>%
+    summarize(AvgTotalSATScore = mean(TotalSatScore),
+              AvgRead = mean(AvgScrRead),
+              AvgMath = mean(AvgScrMath),
+              AvgWrite = mean(AvgScrWrite))
+  
+    plot <- ggplot(df,
+                   aes(x = income_bin, y = y_axis)) + 
+      geom_bar(stat = "identity", fill = "DarkBlue") +
+      xlab("Income Bin") +
+      ylab(y_axis)
+    
+    return(plot)
+}
+
+binned_income_interactive(cali_hs_sat_zip_df, 20000, "AvgTotalSATScore")
