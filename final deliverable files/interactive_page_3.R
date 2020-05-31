4c83f8f969c9fab6c2fb78fd1894638e1e83e0ed
ejmv1 <ejmv1@uw.edu>
2020-05-30 23:36:21 -0700

    progress on third page

--- final deliverable files/interactive_page_3.R
+++ final deliverable files/interactive_page_3.R
@@ -29,34 +29,51 @@ build_third_plot(cali_hs_sat_zip_df, x_axis)
 
 #Making Binned Bar Graph
 
-binned_income_interactive <- function(dataframe, bin) {
+binned_income_interactive <- function(dataframe, bin, yvar = "AvgTotalSATScore") {
   df <- dataframe %>%
     filter(MedianHouseholdIncome != is.na(MedianHouseholdIncome)) %>%
     mutate(income_bin_primary =
              if(min(floor(MedianHouseholdIncome / bin)) == 0){
                (floor(MedianHouseholdIncome / bin) + 1)
              } else {
-                (floor(MedianHouseholdIncome / bin))
-              }) %>%
+               (floor(MedianHouseholdIncome / bin))
+             }) %>%
     mutate(income_bin =
              ifelse(income_bin_primary > 10,
                     10,
                     income_bin_primary)
-           ) %>%
+    ) %>%
     group_by(income_bin) %>%
     summarize(AvgTotalSATScore = mean(TotalSatScore),
               AvgRead = mean(AvgScrRead),
               AvgMath = mean(AvgScrMath),
               AvgWrite = mean(AvgScrWrite))
   
-    plot <- ggplot(df,
-                   aes(x = income_bin,
-                       y = AvgTotalSATScore)) + 
-      geom_bar(stat = "identity", fill = "DarkBlue") +
-      xlab("Income Bin") +
-      ylab("Average Total SAT Score")
-    
-    return(plot)
+  if(yvar == "AvgTotalSATScore"){
+    y_lim <- 2400
+    title <- "SAT Score: Total by Income Bin"
+    y_title <- "Total SAT Score"
+  } else {
+    y_lim <- 800
+    title <- paste("SAT Score:",
+                   substr(yvar,4,nchar(yvar)),
+                   "by Income Bin")
+    y_title <- paste(substr(yvar,4,nchar(yvar)),
+                     "SAT Score")
+  }
+  
+  plot <- plot_ly(df,
+    x = df$income_bin,
+    y = df[[yvar]],
+    type = "bar") %>% 
+    layout(title = title,
+           xaxis = list(title = "Income Bin",
+                        zeroline = FALSE),
+           yaxis = list(title = y_title,
+                        zeroline = FALSE,
+                        range = c(0, y_lim)))
+  
+  return(plot)
 }
 
-binned_income_interactive(cali_hs_sat_zip_df, 25000)
+binned_income_interactive(cali_hs_sat_zip_df, 40000, yvar = "AvgMath")
