60d991498cec49b5d61dc2b984a876c11db324ea
Tony Fei <satouakatsuki@gmail.com>
2020-05-31 16:04:51 -0700

    Made the app runnable

--- final deliverable files/app_ui.R
+++ final deliverable files/app_ui.R
@@ -14,6 +14,7 @@ source("summary_table_page.R")
 page_one_sidepanel <- sidebarPanel(
   "Contents",
   selectInput(
+    "p1_content",
     "Choose from the following options:",
     choices = list("Purpose of the project" = "purpose",
                    "About the project" = "about",
@@ -23,7 +24,8 @@ page_one_sidepanel <- sidebarPanel(
 page_one_mainpanel <- mainPanel(
   "Overview of the Project",
   tags$p(textInput(
-    inputId = "overview"
+    inputId = "overview",
+    label = NULL
   )),
   tags$img(src = "https://www.fccnn.com/incoming/4676816-7wuexj-SAT/alternates/BASE_LANDSCAPE/SAT"),
   tags$img(src = "https://sierranewsonline.com/wp-content/uploads/2018/01/California-Department-of-Education-e1516666248141.png")
@@ -38,6 +40,7 @@ page_one <- tabPanel(
 page_two <- fluidPage(
   h1("Summary Table"),
   tags$p(textInput(
+    label = NULL,
     inputId = "sum_table"
   )),
   tableOutput(
@@ -52,6 +55,7 @@ page_three <- tabPanel(
 
 page_six_sidepanel <- sidebarPanel(
   selectInput(
+    "p6_content",
     h2("Contents:"),
     choices = list("Summary Table" = "summary_table",
                    "Map" = "first_visualisation",
@@ -63,21 +67,22 @@ page_six_sidepanel <- sidebarPanel(
 
 page_six_mainpanel <- mainPanel(
   tags$p(textInput(
-    inputId = "takeaway"
+    inputId = "takeaway",
+    label = NULL
   ))
 )
 
-page_six <- tabPanel(
-  h1("Project Takeaways"),
-  page_five_sidepanel,
-  page_five_mainpanel
-)
+# page_six <- tabPanel(
+#   h1("Project Takeaways"),
+#   page_five_sidepanel,
+#   page_five_mainpanel
+# )
 ui <- navbarPage(
   "Standardised Testing and Median Household Income in California",
   page_one,
   page_two,
   page_three,
-  page_four,
-  page_five,
-  page_six
+  page_four
+  # page_five,
+  # page_six
 )
